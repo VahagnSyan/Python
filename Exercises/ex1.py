d72bdbbe32d9bd20ba607471c9c4afa2e40e1ca2
Ani <tamaryanani950@gmail.com>
2023-11-29 14:16:29 +0400

    Exercise 1

---
+++ Exercises/ex1.py
@@ -0,0 +1,13 @@
+'''
+ex.1
+Display numbers divisible by 5 from a list
+
+Given list is  [10, 20, 33, 46, 55]
+Divisible by 5
+10
+'''
+
+input_list = [10, 2, 30, 40]
+for i in input_list:
+    if(i % 5 == 0):
+        print(i)
