1f5e5f6234cf687a76397f978822cd9770ade29e
Karlen <karlen.matevosyan.ec@gmail.com>
2023-11-29 21:00:54 +0400

    Removing first n characters from string

---
+++ 28-11-2023/remove_first_n.py
@@ -0,0 +1,9 @@
+'''
+The program removes first n characters from string
+'''
+
+
+name = 'Heriqnaz'
+rm_count = int(input("Enter the n: "))
+print("Your name -", name)
+print(f"Removed first {n} -", name[rm_count:])  # print name from n to up
