27618c7f9b1d6062c36c1370a9464885646ee205
Vahag <sargsyanvahag6@gmail.com>
2023-12-02 21:41:27 +0400

    Exercise 11

--- Homework_2/Exercise_11.py
+++ Homework_2/Exercise_11.py
@@ -9,20 +9,12 @@ import random
 
 def matrix_addition(row, column):  # Return the matrix with random numbers
     result_matrix = []
-    for _ in range(row):
-        middle_matrix = []  # Generate a middle matrix with random numbers
+    for i in range(row):
+        result_matrix.append([])  
         for _ in range(column):
-            middle_matrix.append(random.randint(1, 9))
-        
-        result_matrix.append(middle_matrix)
+            result_matrix[i].append(random.randint(0, 9))
     
     return result_matrix
-
-def print_matrix(matrix):  # Print the 2 dimensional matrix elements
-    for i in matrix:
-        for j in i:
-            print(j, end=" ")
-        print()
 
 matrix_row = input("Enter the number of rows: ")
 while not matrix_row.isdigit():
@@ -33,4 +25,4 @@ while not matrix_column.isdigit():
     matrix_column = input("Enter the number of columns: ")
 
 matrix = matrix_addition(int(matrix_row), int(matrix_column))
-print_matrix(matrix)
+print(matrix)
