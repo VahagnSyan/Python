66b093bf03e95502490ece2fef4862bebfbde9f9
NareRamazyan <128586114+NareRamazyan@users.noreply.github.com>
2023-12-03 19:51:23 +0400

    Update even_index_characters.py

--- even_index_characters.py
+++ even_index_characters.py
@@ -1,9 +1,11 @@
 """
-ex.3
-Print characters from a string that are present at an even index number
-Orginal String is  test_text
-Printing only even index chars
+    ex.3
+    Print characters from a string that are present at an even index number
+    Orginal String is  test_text
+    Printing only even index chars
 """
+
+
 string = "test_text"
 iven_index_chars = string[::2]
 print("original string:", string)
