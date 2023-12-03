60b8d093348b31f9948eaa306f6c8df2cb50f88d
ManeShakhkyan <135702736+ManeShakhkyan@users.noreply.github.com>
2023-12-04 01:04:33 +0400

    Updated

--- Homeworks_6_to_13/task_7.py
+++ Homeworks_6_to_13/task_7.py
@@ -1,17 +1,18 @@
 '''
-    ex.8
-    endswith(str_obj, sub_string)
+    task 7
+    startswith(str_obj, sub_string)
 '''
 
-def endswith(str_obj, sub_string):
-    return str_obj[len(sub_string):] == sub_string
+def startswith(str_obj, sub_string):
+    return str_obj[:len(sub_string)] == sub_string
 
 string_obj = input('Enter any string: ')
 substring = input('Enter the substring: ')
 
-print(f'\nChecking if "{string_obj}" ends with "{substring}"?...')
+print(f'\nChecking if "{string_obj}" starts with "{substring}"?...')
 
-if endswith(string_obj, substring):
+if startswith(string_obj, substring):
     print('Result ---> TRUE')
 else:
     print('Result ---> FALSE')
+
