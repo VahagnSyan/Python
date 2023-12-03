7048e04c9713f813b72a48917d9ed4775fc5055d
NareRamazyan <128586114+NareRamazyan@users.noreply.github.com>
2023-12-03 19:51:55 +0400

    Update is_in_str.py

--- is_in_str.py
+++ is_in_str.py
@@ -1,9 +1,10 @@
 """
-ex.9
-write function for the following string operation
-is_in_str(str_obj, sub_string)
-this function should do the same as a builtin string function do
+    ex.9
+    write function for the following string operation
+    is_in_str(str_obj, sub_string)
+    this function should do the same as a builtin string function do
 """
+
 
 def is_in_str(str_obj, sub_string):
     if sub_string in str_obj:
