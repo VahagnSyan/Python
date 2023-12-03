f5f9d3982b6ad5aa6e10b187ef665c65d5d1d7a5
NareRamazyan <128586114+NareRamazyan@users.noreply.github.com>
2023-12-03 19:53:07 +0400

    Update starts_with.py

--- starts_with.py
+++ starts_with.py
@@ -1,9 +1,10 @@
 """
-ex.7
-write function for the following string operation
-startswith(str_obj, sub_string)
-this function should do the same as a builtin string function do
+    ex.7
+    write function for the following string operation
+    startswith(str_obj, sub_string)
+    this function should do the same as a builtin string function do
 """
+
 
 def starts_with(str_obj, sub_string):
     if len(sub_string) > len(str_obj):
