336c903b74b3d5838864d956358b1e5e0e8162f3
NareRamazyan <128586114+NareRamazyan@users.noreply.github.com>
2023-12-03 19:46:32 +0400

    Update 1_22_333.py

--- 1_22_333.py
+++ 1_22_333.py
@@ -1,10 +1,12 @@
 """
-ex.4
-Print the following pattern
-1 
-2 2 
-3 3 3 
+    ex.4
+    Print the following pattern
+    1 
+    2 2 
+    3 3 3 
 """
+
+
 rows = int(input("rows = "))
 
 for i in range(1, rows + 1):
