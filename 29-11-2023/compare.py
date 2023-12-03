d1b9079da0041df0528f50e410aed7c7c6fd9ee6
Karlen <karlen.matevosyan.ec@gmail.com>
2023-12-03 15:49:03 +0400

    Fix: Return max of two numbers

--- 29-11-2023/compare.py
+++ 29-11-2023/compare.py
@@ -1,14 +1,5 @@
-def compare_numbers(num1, num2):
-    difference = num1 - num2
-    sign = (difference > 0) - (difference < 0)
-
-    if sign == 1:
-        print(f"{num1} is greater than {num2}")
-    elif sign == 0:
-        print(f"{num1} is equal to {num2}")
-    else:
-        print(f"{num2} is greater than {num1}")
-
+def max_of_two_numbers(num1, num2):
+    return int((abs(num1 - num2) + num1 + num2) / 2)
 while True:  # infinite loop untill user input is valid
     try:     # trying to catch exceptions
         num1 = int(input("Enter the first integer: "))
@@ -17,4 +8,5 @@ while True:  # infinite loop untill user input is valid
     except ValueError:   # this line is responsible for catching exceptions
         print("Invalid input. Please enter a valid integer.")   # if exception was caught write
                                                                 # helping msg to user
-compare_numbers(num1, num2)
+result = max_of_two_numbers(num1, num2)
+print("The Max number is:", result)
