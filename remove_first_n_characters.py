a5b54a44ab3edff9d88fb2cbe72709bed424f00c
NareRamazyan <128586114+NareRamazyan@users.noreply.github.com>
2023-12-03 19:52:47 +0400

    Update remove_first_n_characters.py

--- remove_first_n_characters.py
+++ remove_first_n_characters.py
@@ -1,8 +1,9 @@
 """
-ex.2
-Remove first n characters from a string
-Write a program to remove characters from a string starting from zero up to n and return a new string.
+    ex.2
+    Remove first n characters from a string
+    Write a program to remove characters from a string starting from zero up to n and return a new string.
 """
+
 
 string = "hello world"
 n = int(input("n = "))
