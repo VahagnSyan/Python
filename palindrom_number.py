b47d5211200980aa9b1114350ca93b39c75e1bc6
NareRamazyan <128586114+NareRamazyan@users.noreply.github.com>
2023-12-03 19:52:16 +0400

    Update palindrom_number.py

--- palindrom_number.py
+++ palindrom_number.py
@@ -1,10 +1,12 @@
 """
-ex.5
-Check Palindrome Number
-Write a program to check if the given number is a palindrome number.
-A palindrome number is a number that is the same after reverse.
-For example, 545, is the palindrome numbers
+    ex.5
+    Check Palindrome Number
+    Write a program to check if the given number is a palindrome number.
+    A palindrome number is a number that is the same after reverse.
+    For example, 545, is the palindrome numbers
 """
+
+
 number = int(input("number = "))
 
 num_str = str(number)
