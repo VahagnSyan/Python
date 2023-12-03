356f4675cb9836db016b160ba3e51d1910d8a490
ManeShakhkyan <mane02072004@gmail.com>
2023-12-03 20:01:54 +0400

    Fixed inputs,and added new homeworks

---
+++ Homeworks_6_to_13/task_12.py
@@ -0,0 +1,21 @@
+'''
+Ex. 12
+Գրեք ֆունկցիա, որը ստանում է ներդրված  ցուցակներ և վերադարձնում է տրված ցուցակի ներդրված ցուցակների բոլոր էլեմենտները պարունակող մի ցուցակ: Օրինակ
+մուտքագրված է  -> [['a', 'b'], ['c', 'd']]
+Աարդյունքը պետք է լինի ['a', 'b', 'c', 'd: '].
+'''
+def flatten_list(nested_list):
+    result_list = []
+
+    for element in nested_list:
+        if type(element) == list:
+            result_list.extend(flatten_list(element))
+        else:
+            result_list.append(element)
+
+    return result_list
+
+original_list = [1, [2, 3], [[4]]]
+result = flatten_list(original_list)
+print(f'Original list is: {original_list}')
+print(f'Flatten list is: {result}')
