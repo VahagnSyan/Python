a3b699a473f40af67c0861512d910be22a6a6d04
Vahag <sargsyanvahag6@gmail.com>
2023-12-04 22:24:18 +0400

    Exercise 14

---
+++ Homework_3/Exercise_14.py
@@ -0,0 +1,15 @@
+# Return dictonary, where keys is text symbols and values are their counts
+def get_symbol_frequency(some_text):
+    result = {}
+    for i in some_text:
+        if i in result:
+            result[i] += 1
+        else:
+            result[i] = 1
+    
+    return result
+
+input_string = input("Enter the some text: ")
+result = get_symbol_frequency(input_string)
+
+print(result)
