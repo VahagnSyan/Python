6d0c304fe7abe4a492ae5bf46ca9f9b57d61073b
saghatelyaannn <arman.saghatelyan.2006@gmail.com>
2023-12-05 15:16:20 +0400

    added charsCount.py

--- cw/charsCountInString.py
+++ cw/charsCountInString.py
@@ -1,19 +1,10 @@
-def element_count(element, string):
-    count = 0
-    for i in string:
-        if i == element:
-            count += 1
-    return count
-
-def create_dict(string):
+def count_elements(string):
     result = {}
     for i in string:
-        if i not in result:
-            char_count = element_count(i, string)
-            result[i] = char_count
+        result[i] = result.get(i, 0) + 1
     return result
 
 string = str(input("Input the line: ")) 
-result_dict = create_dict(string)
+result_dict = count_elements(string)
 print(result_dict)
 
