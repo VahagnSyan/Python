d70cd927267abf4a5572a30f48565aa041f01406
Gegham <gegham.hay@gmail.com>
2023-12-05 12:53:59 +0400

    add ex 10 and 11

--- ex_10.py
+++ ex_10.py
@@ -1,5 +1,17 @@
-# Գրեք այնպիսի ֆունկցիա, որը ստանում է  ամբողջ թիվ 
+# Գրեք այնպիսի ֆունկցիա, որը ստանում է  ամբողջ թիվ
 # և տպում այդ թվից փոքր կամ հավասար բոլոր պարզ թվերը:
 # Պարզ թիվը 1-ից մեծ դրական ամբողջ թիվ է, որը բաժանվում է միայն իր և 1-ի վրա:
 
 
+def is_prime_number(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+
+try:
+    number = int(input("Enter Number :"))
+    print(is_prime_number(number))
+except ValueError:
+    print("Error: Please enter a valid integer for the number ")
--- ex_11.py
+++ ex_11.py
@@ -1,3 +1,32 @@
 # Գրեք ֆունկցիա, որը ստեղծում է ամբողջ թվերի մատրիցա։
 # Մուտքագրվում է երկու m և n թվեր և ստեղծում է m x n չափի մատրիցա,
 # որը լցված է 0-ից 9-ի միջև պատահական ամբողջ թվերով:
+
+
+from random import randint
+from os import system
+from time import sleep
+
+try:
+    Row = int(input("Enter the number of rows:"))
+    Column = int(input("Enter the number of columns:"))
+
+    matrix = []
+
+    for row in range(Row):
+        a = []
+
+        for column in range(Column):
+            a.append(randint(0, 100))
+        matrix.append(a)
+
+    for row in range(Row):
+        for column in range(Column):
+            print(matrix[row][column], end=" ")
+        print()
+
+except:
+    print("Error: Please enter a valid integer for the number ")
+    sleep(2)
+    system("clear")
+    system("python3 ex_11.py")
