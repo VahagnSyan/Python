8dfc6bbc98ba3ce94d0035eeac88e31e5b126fa7
a_nyut <anyuta.andrea@gmail.com>
2023-12-06 14:23:00 +0400

    task 15 - CHANGED

--- ex15.py
+++ ex15.py
@@ -3,13 +3,14 @@
 function counts how many times 
 does the same word occur in sentence.!
 '''
+import re
 
-text = 'Government of the people, by the people, and for the people, shall not perish from the earth.'
+text = "Government of the people, by the people, and for the people. shall not perish from the earth."
     #get separated words in list, then iterating in list
-my_list = text.split(" ")
+arr = re.split("[ ,.]", text)
 #print(my_list)
 dic = {}
-for i in my_list:
+for i in arr:
     if i in dic:
         count = dic[i]
         count = count+1
