ae502a2e51a27f97b73e325c0daa8d6c4c4b3b51
NareRamazyan <128586114+NareRamazyan@users.noreply.github.com>
2023-12-10 14:26:21 +0400

    Add files via upload

---
+++ guess_the_num.py
@@ -0,0 +1,19 @@
+from random import randint
+
+target = randint(1, 100)
+print("guess the number from 1 to 100")
+
+while True:
+    input_number = input("Enter the number: ")
+    if input_number.isdigit():
+        input_num = int(input_number)
+        if input_num == target:
+            print("Congratulations! You guessed the number correctly!")
+            break
+        elif input_num > target:
+            print("Try a smaller number.")
+        else:
+            print("Try a larger number.")
+    else:
+        print("Invalid input. Please enter a valid number.")
+
