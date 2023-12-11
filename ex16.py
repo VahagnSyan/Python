304a34019266d2b86ef18405922e9424cf6ea91d
alvardhovakimyan <152319655+alvardhovakimyan@users.noreply.github.com>
2023-12-11 15:16:38 +0400

    Update ex16.py

--- ex16.py
+++ ex16.py
@@ -6,10 +6,10 @@ Write a function that takes a list with repeating elements and returns a new lis
 def remove_duplicates(input_list):
     unique_elements = []
     for element in input_list:
-        if element not in unique_elements:
+        if input_list.count(element) == 1:
             unique_elements.append(element)
     return unique_elements
 
-original_list = [1, 2, 2, 6,3, 4, 4, 5, 3, 4, 4, 5]
+original_list = [1, 2, 2, 6, 3, 4, 4, 5, 3, 4, 4, 5]
 result_list = remove_duplicates(original_list)
 print(result_list)
