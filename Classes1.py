6150b3044921d106d5f6d98cad9cbe764384773a
alvardhovakimyan <152319655+alvardhovakimyan@users.noreply.github.com>
2023-12-21 22:21:47 +0400

    Update Classes1.py

--- Classes1.py
+++ Classes1.py
@@ -65,8 +65,24 @@ class SportsCar(PassengerCar):
 
     def customize_car(self, decal):
         return f"Customizing the sports car with a decal: {decal}"
-        
-#function when use input test is integer
+
+class Van(Truck):
+    def __init__(self, tires, fuel, max_speed, cargo_capacity, passenger_capacity):
+        super().__init__(tires, fuel, max_speed, cargo_capacity)
+        self.passenger_capacity = passenger_capacity
+
+    def carry_passengers(self):
+        return f"The van is carrying {self.passenger_capacity} passengers."
+
+class PickupTruck(Truck):
+    def __init__(self, tires, fuel, max_speed, cargo_capacity, towing_capacity):
+        super().__init__(tires, fuel, max_speed, cargo_capacity)
+        self.towing_capacity = towing_capacity
+
+    def tow_trailer(self):
+        return f"The pickup truck is towing a trailer with a capacity of {self.towing_capacity} tons."
+
+# Function to get numeric input
 def get_numeric_input(prompt):
     while True:
         try:
@@ -75,7 +91,7 @@ def get_numeric_input(prompt):
         except ValueError:
             print("Invalid input. Please enter a numeric value.")
 
-
+# Create instances  PassengerCar, Van, and PickupTruck 
 tires_passenger_car = 4
 fuel_passenger_car = 50
 max_speed_passenger_car = 180
@@ -83,7 +99,28 @@ passengers_passenger_car = 4
 
 passenger_car = PassengerCar(tires=tires_passenger_car, fuel=fuel_passenger_car, max_speed=max_speed_passenger_car, passengers=passengers_passenger_car)
 
+# Create instances  Van and PickupTruck
+van = Van(tires=4, fuel=60, max_speed=150, cargo_capacity=3, passenger_capacity=8)
+pickup_truck = PickupTruck(tires=6, fuel=70, max_speed=120, cargo_capacity=5, towing_capacity=2)
+
+#  PassengerCar
 print(passenger_car.drive(hours=2, speed=60))
 print(passenger_car.play_music())
 print(passenger_car.passenger_count())
+print()
 
+# Van
+print(van.drive(hours=1, speed=50))
+print(van.load_cargo())
+print(van.check_cargo_capacity())
+print(van.spray_paint(color="blue"))
+print(van.carry_passengers())
+print()
+
+# PickupTruck
+print(pickup_truck.drive(hours=2, speed=80))
+print(pickup_truck.load_cargo())
+print(pickup_truck.check_cargo_capacity())
+print(pickup_truck.spray_paint(color="red"))
+print(pickup_truck.tow_trailer())
+print()
