9527d5ffd507e7cabf4f448e07398cf13dd24975
alvardhovakimyan <152319655+alvardhovakimyan@users.noreply.github.com>
2024-01-09 22:08:16 +0400

    Update and rename ex 23.py to ex23.py

--- ex23.py
+++ ex23.py
@@ -3,19 +3,16 @@ def my_range(start, stop=None, step=1):
         stop = start
         start = 0
 
-    result = []
     current = start
 
     if step > 0:
         while current < stop:
-            result.append(current)
+            yield current
             current += step
     elif step < 0:
         while current > stop:
-            result.append(current)
+            yield current
             current += step
-
-    return result
 
 for i in my_range(2, 10, 2):
     print(i)
