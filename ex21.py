b407d9439fe2d22e38f39a9d6a253052ff9fd280
alvardhovakimyan <152319655+alvardhovakimyan@users.noreply.github.com>
2024-01-09 13:22:33 +0400

    Added ex21.py

---
+++ ex21.py
@@ -0,0 +1,14 @@
+def mmap(func, iterable1, iterable2):
+    for item1, item2 in zip(iterable1, iterable2):
+        yield func(item1, item2)
+
+def add(num1, num2):
+    return num1 + num2
+
+l1 = [1, 2, 3, 4, 5]
+l2 = [1, 3, 5]
+
+result_iterator = mmap(add, l1, l2)
+
+for result in result_iterator:
+    print(result)
