ae3db2a4a4e1bb60b71776e38da630f4bc3c3a54
Vahag <sargsyanvahag6@gmail.com>
2024-01-09 19:52:26 +0400

    Change map function

--- map.py
+++ map.py
@@ -5,18 +5,21 @@
 
 from operator import add
 
-def mapp(func, first_list, second_list):
+
+def mapp(func, *iterables):
     '''
-    Make an generator for two lists
+    Return a generator that computes the function using arguments from
+    each of the iterables.
     '''
-    first_it = iter(first_list)
-    second_it = iter(second_list)
+    iterators = [iter(i) for i in iterables]
     while True:
         try:
-            yield func(next(first_it), next(second_it))
+            yield func(*[next(it) for it in iterators])
         except StopIteration:
             break
+
+
 list_1 = [1, 2, 3]
 list_2 = [4, 5, 6]
 
-print(list(mapp(add, list_1, list_2)))
+print(list(mapp(add, list_1, list_2, [5, 8, 9])))
