914eb021c79a11a1e44fb38d0038baf5e9812daa
Karlen <karlen.matevosyan.ec@gmail.com>
2024-01-10 18:38:36 +0400

    Updated my range function

--- my_range.py
+++ my_range.py
@@ -1,11 +1,32 @@
-def my_range(start, end, step=1):
-    if start > end:
-        while start > end:
-            yield start
-            start -= step
-    elif start < end:
-        while start < end:
-            yield start
-            start += step
-    else:
-        yield []
+def my_range(start, end=None, step=1):
+    if end == None:
+        end = start
+        start = 0
+    while start > end:
+        yield start
+        start -= step
+    while start < end:
+        yield start
+        start += step
+
+
+assert list(my_range(5)) == [0, 1, 2, 3, 4]
+
+assert list(my_range(2, 8, 2)) == [2, 4, 6]
+
+assert list(my_range(10, 5)) == [10, 9, 8, 7, 6]
+
+assert list(my_range(5, 5)) == []
+
+assert list(my_range(0, 10, 3)) == [0, 3, 6, 9]
+
+assert list(my_range(10, 0)) == [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+assert list(my_range(5, 2)) == [5, 4, 3]
+
+assert list(my_range(3, 3)) == []
+
+assert list(my_range(0, -5)) == [0, -1, -2, -3, -4]
+
+print("All assertions passed!")
+
