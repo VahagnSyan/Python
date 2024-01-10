45646b6158f9c64f9eb64cd495e708f60845b3ef
Karlen <karlen.matevosyan.ec@gmail.com>
2024-01-10 19:07:16 +0400

    Fixed repeat function

--- my_repeat.py
+++ my_repeat.py
@@ -1,10 +1,9 @@
 def my_cycle(itearable, count):
     for _ in range(count):
-        for i in itearable:
-            yield  i
+        yield itearable
 
-   
-a = my_cycle([1, 2], 2)
+
+a = my_cycle("aaa", 2)
 print(next(a))
 print(next(a))
 print(next(a))
