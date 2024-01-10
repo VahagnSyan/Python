779dbb91aa14f41a2909175073824ed0195567f0
alvardhovakimyan <152319655+alvardhovakimyan@users.noreply.github.com>
2024-01-10 13:55:22 +0400

    Update ex22.py

--- ex22.py
+++ ex22.py
@@ -9,13 +9,12 @@ def is_even(num):
 def is_vowel(symbol):
     return symbol.lower() in 'aeiou'
 
-text = 'ejk vknkn fdvkd jwd,s sa;jbdas'
-result = list(filter(is_vowel, text))
-print(f"for text")
-print(result)
-print()
-
+text = "ejk vknkn fdvkd jwd,s sa;jbdas"
 l = [1, 2, 3, 5, 6]
-result = list(filter(is_even, l))
-print(f"for number")
-print(result)
+
+# Assertions
+assert list(filter(is_even, l)) == [2, 6], "Answer must be [2, 6]"
+assert list(filter(is_vowel, text)) == ['e', 'a', 'a'], "Answer must be ['e', 'a', 'a']"
+
+l = [2.3, 2, 5, 6]
+assert list(filter(is_even, l)) == [2, 6], "Answer must be [2, 6]"
