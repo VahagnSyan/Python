95ce9c51a7820b6dec94dda36814395a0f447ca0
Kamo <kamo.harutyunyan.ec@gmail.com>
2024-01-15 14:43:23 +0400

    Updated range class

--- my_range_with_class.py
+++ my_range_with_class.py
@@ -1,7 +1,7 @@
 class MyRange:
     def __init__(self, *args):
         if not args:
-            raise ValueError("You dont insert any arguments.")
+            raise ValueError("You didn't insert any arguments.")
         elif len(args) > 3:
             raise ValueError("Too many arguments.")
         elif len(args) == 1:
@@ -15,23 +15,25 @@ class MyRange:
             self.start, self.end, self.step = args
 
     def __iter__(self):
-        while (self.start < self.end and self.step > 0) or \
+        return self
+
+    def __next__(self):
+        if (self.start < self.end and self.step > 0) or \
               (self.start > self.end and self.step < 0):
-            yield self.start
+            result = self.start
             self.start += self.step
+            return result
+        else:
+            raise StopIteration
 
     def __eq__(self, other):
         return list(self) == other
 
 
-assert MyRange(5) == [0, 1, 2, 3, 4]
-assert MyRange(2, 8, 2) == [2, 4, 6]
-assert MyRange(10, 5) == []
-assert MyRange(5, 5) == []
-assert MyRange(0, 10, 3) == [0, 3, 6, 9]
-assert MyRange(10, 5, -1) == [10, 9, 8, 7, 6]
-assert MyRange(5, 2, -2) == [5, 3]
-assert MyRange(3, 3) == []
-assert MyRange(0, 5, -1) == []
+assert list(MyRange(5)) == [0, 1, 2, 3, 4]
+assert list(MyRange(1, 5)) == [1, 2, 3, 4]
+assert list(MyRange(1, 10, 2)) == [1, 3, 5, 7, 9]
+assert list(MyRange(10, 1, -1)) == [10, 9, 8, 7, 6, 5, 4, 3, 2]
+assert list(MyRange(10, 1, 1)) == []
+assert list(MyRange(3, 30, 3)) == [3, 6, 9, 12, 15, 18, 21, 24, 27]
 print("All assertions passed!")
-
