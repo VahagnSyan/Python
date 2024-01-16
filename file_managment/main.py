b15ebdd45bda054cf6856e11d964fc8b02e79c80
Kaammoo <115418178+Kaammoo@users.noreply.github.com>
2024-01-17 00:05:01 +0400

    Update main.py

--- file_managment/main.py
+++ file_managment/main.py
@@ -12,17 +12,20 @@ with open("random_words.txt", 'w') as file:
         file.write(random_word + '\n')
 
 with open("random_words.txt", 'r') as file:
-        unique_words = open("unique_words.txt", 'w')
-        same_words = open("same_words.txt", 'w')
         same_words_set = set()
         file_words = file.readlines()
         unique_words_set = set()
         for word in file_words:
-            if word not in unique_words_set:
-                unique_words.write(word)
-                unique_words_set.add(word)
-            elif word not in same_words_set:
-                same_words.write(word)
+            if word in unique_words_set:
                 same_words_set.add(word)
-        unique_words.close()
-        same_words.close()
+                unique_words_set.remove(word)
+            else:
+                unique_words_set.add(word)
+
+        with open("unique_words.txt", 'w') as unique_words:
+            for word in unique_words_set:
+                unique_words.write(word)
+
+        with open("same_words.txt", 'w') as same_words:
+            for word in same_words_set:
+                same_words.write(word)
