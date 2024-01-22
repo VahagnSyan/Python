3a0d9ae62d32cc48aad94e3baaab88d32b48ff91
Sargis Ghalechyan <sargis.ghalechyan1@gmail.com>
2024-01-22 22:20:16 +0400

    del

---
+++ Namespace_Scope/del/test.py
@@ -0,0 +1,14 @@
+a = 10
+
+
+def dell(obj: str):
+    '''
+    My variant for del keyword
+    '''
+    try:
+        globals().pop(obj)
+    except KeyError as ke:
+        raise NameError(f"name '{obj}' is not defined") from ke
+
+
+dell('a')
